6e6a40602594e6c233d2404551471422b9fe27fe
shengj.sang <shengj.sang@icloud.com>
2022-12-06 18:05:15 +0800

    feat: add .gitigonre

--- target/debug/app.d
+++
@@ -1,1 +0,0 @@
-/Users/shengj.sang/Documents/1-Code/Rust/Axum-Admin/target/debug/app: /Users/shengj.sang/Documents/1-Code/Rust/Axum-Admin/app/src/main.rs
--- target/debug/deps/app-218f59062b5f4c46.d
+++
@@ -1,5 +0,0 @@
-/Users/shengj.sang/Documents/1-Code/Rust/Axum-Admin/target/debug/deps/app-218f59062b5f4c46: app/src/main.rs
-
-/Users/shengj.sang/Documents/1-Code/Rust/Axum-Admin/target/debug/deps/app-218f59062b5f4c46.d: app/src/main.rs
-
-app/src/main.rs:
