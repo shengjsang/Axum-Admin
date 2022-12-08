6a987b47a82fa568edc648f7c4379466b9cd9675
shengj.sang <shengj.sang@icloud.com>
2022-12-08 09:31:42 +0800

    feat: 集成axum

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,13 +3,17 @@ use axum::{
     Router,
 };
 
+
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    // route函数来设置路由，两个参数 一个路由路径 一个handler函数
+    // handler函数就是一个异步函数来处理程序逻辑，从请求中提取解析作为参数，并返回响应，响应要实现IntoResponse
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // run it with hyper on localhost:3000
+
+    // 设置端口
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        // 服务启动
         .serve(app.into_make_service())
         .await
         .unwrap();
